a2cdd41326c464de56eb97869d8ac264ce849588
Christian Turkoanje <crisss1205@gmail.com>
2017-02-21 13:58:19 -0500

    Changed resources for iD Tech

--- jethost_install.sh
+++ jethost_install.sh
@@ -1,28 +1,28 @@
 #Ros Prerequisites
 sudo update-locale LANG=C LANGUAGE=C LC_ALL=C LC_MESSAGES=POSIX
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu xenial main" > /etc/apt/sources.list.d/ros-latest.list'
 wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
 sudo apt-get update
 
-#Ros Jade Base
-sudo apt-get -y install ros-jade-desktop-full
+#Ros kinetic Base
+sudo apt-get -y install ros-kinetic-desktop-full
 
 #Python Dependencies
 sudo apt-get -y install python-rosdep python-dev python-pip python-rosinstall python-wstool
 
 sudo rosdep init
 rosdep update
-echo "source /opt/ros/jade/setup.bash" >> ~/.bashrc
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
 #Ros packages
-sudo apt-get -y install ros-jade-rosserial-arduino
-sudo apt-get -y install ros-jade-rosserial
-sudo apt-get -y install ros-jade-joy
-sudo apt-get -y install ros-jade-teleop-twist-joy
+sudo apt-get -y install ros-kinetic-rosserial-arduino
+sudo apt-get -y install ros-kinetic-rosserial
+sudo apt-get -y install ros-kinetic-joy
+sudo apt-get -y install ros-kinetic-teleop-twist-joy
 
 # Configure Catkin Workspace
-source /opt/ros/jade/setup.bash
+source /opt/ros/kinetic/setup.bash
 cd ../../
 catkin_init_workspace
 
